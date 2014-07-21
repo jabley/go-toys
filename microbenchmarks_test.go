dad2edeb716b4fb052672b83935a9ea10835931e
James Abley <james.abley@gmail.com>
2014-07-21 12:46:59 +0100

    Rename method
    Easier to specify a regexp when running `-bench` with similar names.

--- microbenchmarks_test.go
+++ microbenchmarks_test.go
@@ -14,7 +14,7 @@ func BenchmarkByteCasts(b *testing.B) {
 	}
 }
 
-func BenchmarkConstantBytes(b *testing.B) {
+func BenchmarkByteConstants(b *testing.B) {
 	checksum := sha1.New()
 	for i := 0; i < b.N; i++ {
 		checksum.Write(trueBytes)
