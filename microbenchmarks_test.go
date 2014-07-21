374c9ef74fea26700cd74bd12f6a2beca59bdc53
James Abley <james.abley@gmail.com>
2014-07-21 12:30:31 +0100

    micro benchmark showing the micro optimisation

---
+++ microbenchmarks_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"crypto/sha1"
+	"testing"
+)
+
+var trueBytes = []byte{'(', 't', 'r', 'u', 'e', ')'}
+
+func BenchmarkByteCasts(b *testing.B) {
+	checksum := sha1.New()
+	for i := 0; i < b.N; i++ {
+		checksum.Write([]byte("(true)"))
+	}
+}
+
+func BenchmarkConstantBytes(b *testing.B) {
+	checksum := sha1.New()
+	for i := 0; i < b.N; i++ {
+		checksum.Write(trueBytes)
+	}
+}
